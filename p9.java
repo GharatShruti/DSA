4696449c67f3f2ff5b8256f6fcf17092809e4a25
GharatShruti <shrutigharat62@gmail.com>
2024-04-13 14:28:30 +0530

    commit

---
+++ p9.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class p9 {
+    public static void main(String[] args) {
+        int[] arr = {9,5,25,84,-4,1};
+        Arrays.sort(arr);
+    System.out.println(arr[arr.length-2]);
+    }
+}
+
